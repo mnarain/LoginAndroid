5302089beb7fda69106ce7bbc85613c0206f3eb2
Maarten Narain <mnarain@qualogy.com>
2022-07-25 21:03:12 -0300

    Initial commit

---
+++ app/src/main/java/sr/ptc/helloandroid/DashboardActivity.java
@@ -0,0 +1,31 @@
+package sr.ptc.helloandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+        loadWelcomeMessage();
+    }
+
+    private void loadWelcomeMessage() {
+        Intent intent = getIntent();
+        String message = intent.getStringExtra("message");
+        TextView textView = (TextView) findViewById(R.id.welcomeMessage);
+        textView.setText(message);
+    }
+
+    public void logoutUser(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+}
